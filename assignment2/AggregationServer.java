ffa937388034e5bc1bb2d996bc7b85f380e934ab
Ben <benjamincyna@gmail.com>
2022-08-31 18:01:58 +0930

    client and server communicating

--- assignment2/AggregationServer.java
+++ assignment2/AggregationServer.java
@@ -5,13 +5,27 @@ public class AggregationServer {
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(6666);
-            Socket s = ss.accept();
-            DataInputStream dis = new DataInputStream(s.getInputStream());
-            String str = (String) dis.readUTF();
-            System.out.println("message= " + str);
-            ss.close();
+            Socket s=ss.accept();  
+            DataInputStream din=new DataInputStream(s.getInputStream());  
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+              
+            String str="",str2="";  
+            while(!str.equals("stop")){  
+            str=din.readUTF();  
+            System.out.println("client says: "+str);  
+            str2=br.readLine();  
+            dout.writeUTF(str2);  
+            dout.flush();  
+            }  
+            din.close();  
+            s.close();  
+            ss.close();  
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+    static String sendToClient() {
+        return "stromng";
     }
 }
--- assignment2/GETClient.java
+++ assignment2/GETClient.java
@@ -1,21 +1,37 @@
-import java.io.*;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.net.*;
 public class GETClient {
     public static void main(String[] args) {
         try {
             Socket s = new Socket("localhost", 6666);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("Hello Server");
+            DataInputStream din=new DataInputStream(s.getInputStream());  
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
 
-           
-            dout.flush();
-            dout.close();
-            s.close();
+            String str="",str2="";  
+            while(!str.equals("stop")){  
+                str=br.readLine();  
+                dout.writeUTF(str);  
+                dout.flush();  
+                str2=din.readUTF();  
+                System.out.println("Server says: "+str2);  
+            }  
+            dout.close();  
+            s.close();  
+            
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-    Integer lamport() {
-        return 0;
+    static String lamport() {
+        return "0";
+    }
+
+    static void get(Socket s) {
+        System.out.println("client");
+        return;
     }
 }
