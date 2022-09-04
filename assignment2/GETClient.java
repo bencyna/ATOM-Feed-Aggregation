c3e3388f920369f8cb49bcde70386c944861a8ac
Ben <benjamincyna@gmail.com>
2022-09-04 16:31:02 +0930

    have client asking to reconnect

--- assignment2/GETClient.java
+++ assignment2/GETClient.java
@@ -1,26 +1,46 @@
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
 import java.net.*;
+
 public class GETClient {
-    public static void main(String[] args) {
+    /**
+     * @param attempts
+     */
+    static void connect(Integer attempts) {
         try {
-            Socket s = new Socket("localhost", 4567 );
-            DataInputStream din=new DataInputStream(s.getInputStream());  
-            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+            Socket s = new Socket("localhost", 4567);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
-            String str2="";  
-            dout.writeUTF("client server");  
-            dout.flush();  
-            str2=din.readUTF();  
-            System.out.println("Server says: "+str2);  
+            String str2 = "";
+            dout.writeUTF("client server");
+            dout.flush();
+            str2 = din.readUTF();
+            System.out.println("Server says: " + str2);
 
-            dout.close();  
-            s.close();  
-            
+            dout.close();
+            s.close();
+
         } catch (Exception e) {
-            System.out.println(e);
+            if (attempts < 3) {
+                System.out.println("Connection failed, trying again...");
+                try {
+                    Thread.sleep(500);
+                }
+                catch(Exception err) {
+                    System.out.println(e);
+                }
+                connect(attempts + 1);
+            } else {
+                System.out.println("Connection failed, unable to get content.");
+            }
         }
     }
+
+    public static void main(String[] args) {
+        connect(0);
+    }
+
     static String lamport() {
         return "0";
     }
