a51243608d0c7a1667984c36ec110243979c9be9
Ben Cyna <benjamincyna@gmail.com>
2022-09-07 18:12:52 +0930

    testing looks good

--- assignment2/test.py
+++ assignment2/test.py
@@ -7,6 +7,31 @@ def basics():
     run(["java", "ContentServer", "AggregationServer:4567", "./input/file1.txt"])
     time.sleep(1)
     run(["java", "GETClient", "AggregationServer:4567"])
+    print("comparing your input and output files...")
+    time.sleep(1)
+    with open("./client_output.txt") as output:
+        with open("./input/file1.txt") as input:
+            input_contents = input.readlines()
+            output_contents = output.readlines()
+            passCount = 0
+            failCount = 0
+            expectedOutputLineNum = 0     
+
+            for line in input_contents: 
+                if expectedOutputLineNum >= len(output_contents):
+                    break
+
+                if len(str(output_contents).strip()) > 0:
+                    if output_contents[expectedOutputLineNum] == input_contents:
+                        print(f"test {expectedOutputLineNum}: \"{line}\" passed")
+                        passCount += 1
+                    else:
+                        print(f"test {expectedOutputLineNum}: \"{line}\" failed")
+                        failCount +=1
+                
+                    expectedOutputLineNum += 1
+            
+            print(f"{passCount} tests passed, {failCount} tests failed, tests completed: {expectedOutputLineNum}/{len(input_contents)}")
     server.kill()
     
 
