278b8c942eed822d1842ed35073490eb674134fe
Ben Cyna <benjamincyna@gmail.com>
2022-10-01 21:04:45 +0930

    trying to get xml passer

--- assignment2/ParseXML.java
+++ assignment2/ParseXML.java
@@ -1,3 +1,15 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+
+
 public class ParseXML {
 
 
@@ -6,8 +18,32 @@ public class ParseXML {
         String newLine = "<" + splitLine[0] + ">" + splitLine[1] + "</" + splitLine[0] + ">";
         return "je";
     } 
-    public void StringToXML(String[] parts, String filePath) {
+    public void StringToXML(String[] parts, String filePath) throws ParserConfigurationException {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        Document xmlDoc = docBuilder.newDocument();
+
+        Element rootElement = xmlDoc.createElement("feed");
+
+        Element mainElement = xmlDoc.createElement("inside1");
+
+        Text productNameText = xmlDoc.createTextNode("inside?");
         
+        Element productName = xmlDoc.createElement("inside1");
+
+        Element name = xmlDoc.createElement("name");
+
+        productName.appendChild(productNameText);
+        mainElement.appendChild(productName);
+        rootElement.appendChild(rootElement);
+        xmlDoc.appendChild(rootElement);
+
+        OutputFormat outFormat = new OutputFormat(xmlDoc);
+        outFormat.setIndenting(true);
+
+
         return;
     } 
 
