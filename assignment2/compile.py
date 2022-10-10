c996ee10740bf2636b706f37f6da68ff70444fad
Ben Cyna <benjamincyna@gmail.com>
2022-10-10 13:21:15 +1030

    different port for AS server

--- assignment2/compile.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run
-import time
-
-print("starting your program...")
-run(["javac", "AggregationServer.java"]);
-run(["javac", "GETClient.java"]);
-run(["javac", "ContentServer.java"]);
-run(["javac", "LamportClock.java"]);
